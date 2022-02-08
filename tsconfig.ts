0d9fe7fdfb64fd310d478994d2419387d04231ef
Chin-Yee Karwehl <chin.yee.karwehl@volkswagen.de>
2022-02-08 11:41:29 +0100

    initial commit

---
+++ tsconfig.ts
@@ -0,0 +1,12 @@
+{
+  "compilerOptions": {
+    "target": "ESNext",
+    "module": "commonjs",
+    "moduleResolution": "node",
+    "noEmit": true,
+    "allowSyntheticDefaultImports": true,
+    "esModuleInterop": true,
+    "forceConsistentCasingInFileNames": true,
+    "strict": true
+  }
+}
